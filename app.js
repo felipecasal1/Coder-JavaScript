636f47d810ae8987220887a5bc68c40646806c73
felipe <felipecasalcornejo@gmail.com>
2023-06-06 19:16:17 -0300

    comienza el proyecto

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const bicicleta1 = 23.000;
+const bicicleta2 = 28.000;
+const bicicleta3 = 53.000;
+let cantidad
+let opcion 
+let total = 0;
+
+console.log ("bienvenidos a la tienda de bicicletas")
+
+
+
+    Option = parseInt(prompt("Que producto desea seleccionar "
+    "bicicleta1: $" + bicicleta1 + "/n" 
+ 
+    ))
+
