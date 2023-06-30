b07a3f13d984ae4ffcadedc3c720861470339f84
felipe <felipecasalcornejo@gmail.com>
2023-06-29 23:55:07 -0300

    segunda pre entrega del proyecto

--- app.js
+++ app.js
@@ -1,67 +1,45 @@
 
 
-alert("bienvenidos a la tienda de bicicletas");
 
-let nombreUsuario;
-let contrasenia;
-let edad;
+ let nombreUsuario;
+ let contrasenia;
+ let edad;
+ function registroCorrecto(){
+     alert("Se introducio un dato correcto... Gracias!!")
+ }
 
-function registroCorrecto(){
-    alert("Se introducio un dato correcto... Gracias!!")
-}
+ nombreUsuario = prompt("Ingrese nombre de usuario");
+ if (nombreUsuario != '') {
+     registroCorrecto()
+ } else {
+     nombreUsuario = prompt("Por favor ingresar un nombre de usuario...");
+ }
 
-// Se pide nombre de usuario
-
-nombreUsuario = prompt("Ingrese nombre de usuario");
-if (nombreUsuario != '') {
-    registroCorrecto()
-} else {
-    nombreUsuario = prompt("Por favor ingresar un nombre de usuario...");
-    
-}
-
-// se pide contraseña
-
-
-contrasenia = prompt("Ingrese la contraseña");
-if (contrasenia != '') {
-    console.log("verificando logeo");
-    registroCorrecto()
-} else {
-    contrasenia = prompt("Por favor ingresa una contrasena");
-}
-
-
-// se pide edad
+ contrasenia = prompt("Ingrese la contraseña");
+ if (contrasenia != '') {
+     console.log("verificando logeo");
+     registroCorrecto()
+ } else {
+     contrasenia = prompt("Por favor ingresa una contrasena");
+ }
+ 
+ edad = prompt("Por favot ingrese su edad ")
+ if (edad != Number) {
+     registroCorrecto()
+ } else {
+     edad= prompt("Por favor ingrese un caracter")
+ };
 
 
-edad = prompt("Por favot ingrese su edad ")
-if (edad != Number) {
-    registroCorrecto()
 
-} else {
-    edad= prompt("Por favor ingrese un caracter")
-}
-
-// se piden productos
-
-alert("Bienvendio " + nombreUsuario + " selecciona los productor que necesites ")
-
-let continuar = 'si';
-let precio;
-let cantidad;
-let total = 0;
+let bicicletas = [ 
+    ("Bicicleta Mountainbike", "Top Mega", 750)
+    ("Bicicleta CerroArriba", "Raleigh", 900)
+    ("Bicicleta BMX", "Olmo", 400)
+    ("Bicicleta DownHill", "Venzo", 500)
+];
 
 
-while (continuar == 'si') {
 
-    console.log('agregando un nuevo item en el carrito de compras...');
 
-    precio = parseFloat(prompt("Items Disponibles  \n 1: Bicicleta, Mountain bike $20,000  \n 2: Bicicleta, Downhill $17,000  \n 3: Bicicleta, Cross Country $24,000  \n Introduza el precio"))
-
-    cantidad = parseInt(prompt('que cantidad necesita : '));
-    total = total + precio * cantidad;
-    continuar = prompt('Desea agregar otro item en el carrito de compras? si/no');
-}
-alert("El total de su carrtio es " + total)
 
