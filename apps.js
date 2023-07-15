cd3cdca177f2964a581d0a1b0c808318316a7753
felipe <felipecasalcornejo@gmail.com>
2023-07-14 21:20:47 -0300

    se agregaron modificaciones para la 3era pre entrega del proyecto

--- apps.js
+++ apps.js
@@ -1,133 +1,132 @@
 
- let nombreUsuario;
- let contrasenia;
- let edad;
- function registroCorrecto(){
-     alert("Se introducio un dato correcto... Gracias!!")
- }
- nombreUsuario = prompt("Ingrese nombre de usuario");
- if (nombreUsuario != '') {
-     registroCorrecto()
- } else {
-     nombreUsuario = prompt("Por favor ingresar un nombre de usuario...");
- }
- contrasenia = prompt("Ingrese la contraseña");
- if (contrasenia != '') {
-     console.log("verificando logeo");
-     registroCorrecto()
- } else {
-     contrasenia = prompt("Por favor ingresa una contrasena");
- }
- edad = prompt("Por favot ingrese su edad ")
- if (edad != Number) {
-     registroCorrecto()
- } else {
-     edad= prompt("Por favor ingrese un caracter")
-  }
- const bicicletas =[
- {nombre: "Bicicleta DownHill" ,marca:"Venzo" ,precio: 500 ,n:0 },
- {nombre: "Bicicleta Mountainbike" ,marca:"Top Mega" ,precio: 750 ,n:1},
- {nombre: "Bicicleta CrossCountry" ,marca:"Trinx" ,precio: 600 ,n:2},
- ]
 
 
-let carrito = document.getElementById("carrito")
 
-let botonBorrar = document.getElementById("borrar")
-let boton = document.querySelector("#miBoton");
+  let nombreUsuario;
+  let contrasenia;
+  let edad;
+  function registroCorrecto(){
+      alert("Se introducio un dato correcto... Gracias!!")    
+  };
+   nombreUsuario = prompt("Ingrese nombre de usuario");
+  if (nombreUsuario != '') {
+      registroCorrecto()    
+  } else {
+      nombreUsuario = prompt("Por favor ingresar un nombre de usuario...")    
+  };
+ localStorage.setItem("Nombre",nombreUsuario);
+  contrasenia = prompt("Ingrese la contraseña");
+  if (contrasenia != '') {
+      console.log("verificando logeo");    
+      registroCorrecto()
+  } else {
+      contrasenia = prompt("Por favor ingresa una contrasena")    
+  };
+  localStorage.setItem("contraseña",contrasenia)
+  edad = prompt("Por favot ingrese su edad ");
+  if (edad != Number) {
+      registroCorrecto()    
+  } else {
+      edad= prompt("Por favor ingrese un caracter")
+   };
+   localStorage.setItem("edad",edad)
 
 
-boton.onclick = () => {
-    let opcion = prompt("ingrese el N de producto")
-    switch (opcion) {
-        case "0":
-            let bicicleta1 = `
-            <td>Bicicleta Mountainbike</td>
-        <td>Marca Top Mega</td>
-        <td>$750.00</td>
-        `
-            carrito.innerHTML += bicicleta1
+const bicicletas = [
+    { nombre: "Bicicleta DownHill", marca: "Venzo", precio: 500 },
+    { nombre: "Bicicleta Mountainbike", marca: "Top Mega", precio: 750 },
+    { nombre: "Bicicleta CrossCountry", marca: "Trinx", precio: 600 },
+    { nombre: "Bicicleta Cerro Arriba", marca: "trek", precio: 800 },
+    { nombre: "Bicicleta BMX", marca: "Sunn", precio: 300 },
+]
 
-break;
-        case "1":
-            let bicicleta2 = `
-            <td>Bicicleta Mountainbike</td>
-            <td>Marca Top Mega</td>
-            <td>$750.00</td>`
-            carrito.innerHTML += bicicleta2
-            break;
-        case "2":
-            let bicicleta3 = `
-             <td>Bicicleta CrossCountry</td>
-            <td>Marca Trinx</td>
-            <td>$600.00</td>`
-            carrito.innerHTML += bicicleta3
-            break;
-        case "3":
-            let bicicleta4 = `
-            <td>Bicicleta CerroArriba</td>
-        <td>Marca Raleigh</td>
-        <td>$900.00</td>`
-            carrito.innerHTML += bicicleta4
-            break;
-        case "4":
-            let bicicleta5 = `    
-                            <td>Bicicleta BMX</td>
-                <td>Marca Olmo</td>
-                <td>$400.00</td>
-                
+let carrito = document.getElementById("carrito");
+
+
+
+
+let boton1 = document.getElementById("boton-1");
+
+
+
+
+ boton1.onclick = () => {
+    let bicicleta1 = `
+                <tr>
+            <td>${bicicletas[0].nombre}</td>
+            <td>${bicicletas[0].precio}</td>
+            <td>${bicicletas[0].marca}</td>
+          </tr>
                 `
-                carrito.innerHTML += bicicleta5
-    }
+    carrito.innerHTML += bicicleta1
+};
 
 
 
 
-    alert("Agregaste un elemento al carrito")
+
+let boton2 = document.getElementById("boton-2");
+
+boton2.onclick = () =>{
+    let bicicleta2 = `
+    <tr>
+            <td>${bicicletas[1].nombre}</td>
+            <td>${bicicletas[1].precio}</td>
+            <td>${bicicletas[1].marca}</td>
+          </tr>
+          `
+          carrito.innerHTML += bicicleta2
+};
+
+
+
+
+
+
+let boton3 = document.getElementById("boton-3");
+
+boton3.onclick = () =>{
+    let bicicleta3 = `
+    <tr>
+            <td>${bicicletas[2].nombre}</td>
+            <td>${bicicletas[2].precio}</td>
+            <td>${bicicletas[2].marca}</td>
+          </tr>
+          `
+          carrito.innerHTML += bicicleta3
+};
+
+let boton4 = document.getElementById("boton-4")
+
+boton4.onclick = () =>{
+    let bicicleta4 = `
+    <tr>
+    <td>${bicicletas[3].nombre}</td>
+    <td>${bicicletas[3].precio}</td>
+    <td>${bicicletas[3].marca}</td>
+  </tr>
+    `
+    carrito.innerHTML += bicicleta4
+};
+
+
+
+let boton5 = document.getElementById("boton-5")
+
+boton5.onclick = () =>{
+    let bicicleta5 = `
+    <tr>
+    <td>${bicicletas[4].nombre}</td>
+    <td>${bicicletas[4].precio}</td>
+    <td>${bicicletas[4].marca}</td>
+  </tr>
+    `
+    carrito.innerHTML += bicicleta5
 }
 
-botonBorrar.onclick = () => {
-    let eliminar =prompt("ingrese el N del producto que quiere eliminar")
-    switch (eliminar) {
-        case "0":
-            let bicicleta1 = `
-            <td>Bicicleta Mountainbike</td>
-        <td>Marca Top Mega</td>
-        <td>$750.00</td>
-        `
-            carrito.innerHTML -= bicicleta1
+let tituloCarrito = document.getElementById("Carrito-h2")
 
-break;
-        case "1":
-            let bicicleta2 = `
-            <td>Bicicleta Mountainbike</td>
-            <td>Marca Top Mega</td>
-            <td>$750.00</td>`
-            carrito.innerHTML -= bicicleta2
-            break;
-        case "2":
-            let bicicleta3 = `
-             <td>Bicicleta CrossCountry</td>
-            <td>Marca Trinx</td>
-            <td>$600.00</td>`
-            carrito.innerHTML -= bicicleta3
-            break;
-        case "3":
-            let bicicleta4 = `
-            <td>Bicicleta CerroArriba</td>
-        <td>Marca Raleigh</td>
-        <td>$900.00</td>`
-            carrito.innerHTML -= bicicleta4
-            break;
-        case "4":
-            let bicicleta5 = `    
-                            <td>Bicicleta BMX</td>
-                <td>Marca Olmo</td>
-                <td>$400.00</td>
-                
-                `
-                carrito.innerHTML -= bicicleta5
-    }
-
-    alert("Borraste un elemento del  carrito")
-}
+let titulocarrito =`
+Carrito de Compra de ${localStorage.getItem(JSONparse("nombre"))}
+`
+tituloCarrito.innerHTML += tituloCarrito
