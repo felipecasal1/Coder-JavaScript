66f2e9b099472fd1062c3e90e0d7129cb92bc346
felipe <felipecasalcornejo@gmail.com>
2023-08-17 18:52:00 -0300

    retoques finales para la re entrega

--- apps.js
+++ apps.js
@@ -30,164 +30,229 @@ botonEnviar.onclick = () => {
         })
 
 
-    }}
+    }
+}
 
-    botonEnviar.onclick = () => {
-        if (contrasenia.value.length < 8) {
-            Swal.fire({
-                icon: 'error',
-                title: 'Contraseña',
-                text: 'La contraseña es muy corta, es menor a 8 caracteres!',
+botonEnviar.onclick = () => {
+    if (contrasenia.value.length < 8) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Contraseña',
+            text: 'La contraseña es muy corta, es menor a 8 caracteres!',
+
+        })
+
+
+    }
+}
+
+
+
+
+
+
+
+form.addEventListener("submit", e => {
+
+    e.preventDefault()
+
+
+    localStorage.setItem("Nombre", nombreUsuario.value);
+
+
+    localStorage.setItem("contraseña", contrasenia.value);
+
+
+
+    localStorage.setItem("edad", edad.value);
+
+
+})
+
+
+
+
+const bicicletas = [
+    { nombre: "Bicicleta DownHill", marca: "Venzo", precio: 500 },
+    { nombre: "Bicicleta Mountainbike", marca: "Top Mega", precio: 750 },
+    { nombre: "Bicicleta CrossCountry", marca: "Trinx", precio: 600 },
+    { nombre: "Bicicleta Cerro Arriba", marca: "trek", precio: 800 },
+    { nombre: "Bicicleta BMX", marca: "Sunn", precio: 300 },
+]
+
+let carrito = document.getElementById("carrito");
+
+let boton1 = document.getElementById("boton-1");
+
+let carritoLocalStorage = [];
+
+
+
+boton1.onclick = () => {
     
-            })
+    let bicicleta1 = `
+    <tr>
+    <td >${bicicletas[0].nombre}</td>
+    <td>${bicicletas[0].precio}</td>
+    <td>${bicicletas[0].marca}</td>
+    </tr>
+    `
+    carrito.innerHTML += bicicleta1
     
-    
-        }}
-    
+    let carritoLocalStorage = carritoLocalStorage+bicicletas[0].nombre;
+   
+    localStorage.setItemItem("carritoLocalSrorage",JSON.stringify(carritoLocalStorage));
+
+};
 
 
 
 
 
+let boton2 = document.getElementById("boton-2");
 
-    form.addEventListener("submit", e => {
-
-        e.preventDefault()
-
-
-        localStorage.setItem("Nombre", nombreUsuario.value);
-
-
-        localStorage.setItem("contraseña", contrasenia.value);
-
-
-
-        localStorage.setItem("edad", edad.value);
-
-
-    })
-
-
-
-
-    const bicicletas = [
-        { nombre: "Bicicleta DownHill", marca: "Venzo", precio: 500 },
-        { nombre: "Bicicleta Mountainbike", marca: "Top Mega", precio: 750 },
-        { nombre: "Bicicleta CrossCountry", marca: "Trinx", precio: 600 },
-        { nombre: "Bicicleta Cerro Arriba", marca: "trek", precio: 800 },
-        { nombre: "Bicicleta BMX", marca: "Sunn", precio: 300 },
-    ]
-
-    let carrito = document.getElementById("carrito");
-
-    let boton1 = document.getElementById("boton-1");
-
-
-
-
-    boton1.onclick = () => {
-
-        let bicicleta1 = `
-                <tr>
-            <td >${bicicletas[0].nombre}</td>
-            <td>${bicicletas[0].precio}</td>
-            <td>${bicicletas[0].marca}</td>
-          </tr>
-                `
-        carrito.innerHTML += bicicleta1
-
-    };
-
-
-
-
-
-    let boton2 = document.getElementById("boton-2");
-
-    boton2.onclick = () => {
-        let bicicleta2 = `
+boton2.onclick = () => {
+    let bicicleta2 = `
     <tr>
             <td>${bicicletas[1].nombre}</td>
             <td>${bicicletas[1].precio}</td>
             <td>${bicicletas[1].marca}</td>
           </tr>
           `
-        carrito.innerHTML += bicicleta2
-    };
+    carrito.innerHTML += bicicleta2
+    let carritoLocalStorage = carritoLocalStorage+bicicletas[1].nombre;
+    localStorage.setItem("carritoLocalStorage",JSON.stringify(carritoLocalStorage));
+    
+};
 
 
 
 
 
 
-    let boton3 = document.getElementById("boton-3");
+let boton3 = document.getElementById("boton-3");
 
-    boton3.onclick = () => {
-        let bicicleta3 = `
+boton3.onclick = () => {
+    let bicicleta3 = `
     <tr>
             <td>${bicicletas[2].nombre}</td>
             <td>${bicicletas[2].precio}</td>
             <td>${bicicletas[2].marca}</td>
           </tr>
           `
-        carrito.innerHTML += bicicleta3
-    };
+    carrito.innerHTML += bicicleta3
+};
 
-    let boton4 = document.getElementById("boton-4")
+let boton4 = document.getElementById("boton-4")
 
-    boton4.onclick = () => {
-        let bicicleta4 = `
+boton4.onclick = () => {
+    let bicicleta4 = `
     <tr>
     <td>${bicicletas[3].nombre}</td>
     <td>${bicicletas[3].precio}</td>
     <td>${bicicletas[3].marca}</td>
   </tr>
     `
-        carrito.innerHTML += bicicleta4
-    };
+    carrito.innerHTML += bicicleta4
+};
 
 
 
-    let boton5 = document.getElementById("boton-5");
+let boton5 = document.getElementById("boton-5");
 
-    boton5.onclick = () => {
-        let bicicleta5 = `
+boton5.onclick = () => {
+    let bicicleta5 = `
     <tr>
     <td>${bicicletas[4].nombre}</td>
     <td>${bicicletas[4].precio}</td>
     <td>${bicicletas[4].marca}</td>
   </tr>
     `
-        carrito.innerHTML += bicicleta5
-    }
+    carrito.innerHTML += bicicleta5
+}
 
 
 
-    let vaciarCarrito = document.getElementById("borrar");
+let vaciarCarrito = document.getElementById("borrar");
 
-    vaciarCarrito.onclick = () => {
-        let vaciarCarrito = `
+vaciarCarrito.onclick = () => {
+    let vaciarCarrito = `
     
     <td>"</td>
     <td></td>
     <td></td>
    
     `
-        carrito.innerHTML -= vaciarCarrito
-    }
+    carrito.innerHTML -= vaciarCarrito
+}
 
 
 
 
-    let tituloCarrito = document.getElementById("Carrito-h2")
 
-    form.addEventListener("submit", e => {
 
-        let nuevoTitulo = ` 
-    <H2 id="Carrito-h2">Carrito de Compras de ${nombreUsuario.value}</H2>
-    `
-        tituloCarrito.innerHTML += nuevoTitulo
 
-    })
+
+const DolarBlue = document.getElementById("dolar-blue")
+const DolarOficial = document.getElementById("dolar-oficial")
+const DolarBolsa = document.getElementById("dolar-bolsa")
+
+fetch("https://dolarapi.com/v1/dolares")
+    .then(response => response.json())
+    .then(response => {
+
+        let tablaDolarBlue = `
+            
+          <div>
+            Compra :${response[1].compra}
+            </div>
+            <div>
+            Venta :${response[1].venta}</td> 
+            </div>
+            <div>
+      Fecha Actualizacion :${response[1].fechaActualizacion}
+      </div>
+             `
+        DolarBlue.innerHTML += tablaDolarBlue
+
+        let tablaDolarOficial = `
+            
+    <div>
+      Compra :${response[0].compra}
+      </div>
+      <div>
+      Venta :${response[0].venta}</td> 
+      </div>
+      <div>
+      Fecha Actualizacion :${response[0].fechaActualizacion}
+      </div>
+       `
+        DolarOficial.innerHTML += tablaDolarOficial
+
+        let tablaDolarBolsa = `
+            
+<div>
+  Compra :${response[2].compra}
+  </div>
+  <div>
+  Venta :${response[2].venta}</td> 
+  </div>
+  <div>
+      Fecha Actualizacion :${response[2].fechaActualizacion}
+      </div>
+   `
+        DolarBolsa.innerHTML += tablaDolarBolsa
+    });
+
+
+
+
+
+
+
+
+
+
+
 
 
